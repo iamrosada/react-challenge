9fa74caa141da7210be0a438e61dd6ce5c7318a7
iamrosada <luisrosada2018@gmail.com>
2022-10-01 18:17:23 +0300

    (FEATURE): created the FormComponent , TitleContainer and ContainerComponent

--- src/pages/SingIn/SingIn.tsx
+++ src/pages/SingIn/SingIn.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
+import { LogoMainSVG } from '../../assets/svgs';
+import LinkTextCustom from '../../components/LinkTextCustom/LinkTextCustom';
+import TitleAuth from '../../components/TitleAuth/titleAuth';
 // import { useNavigate } from 'react-router-dom';
 // import { useForm, SubmitHandler } from 'react-hook-form';
-import styles from './scss/_singIn.module.css';
+import styles from './scss/_singIn.module.scss';
 
 const SectionComponent: React.FC = ({ children }) => {
   return <section className={styles.sectionComponent}>{children}</section>;
 };
 
+const ContainerComponent: React.FC = ({ children }) => {
+  return <div className={styles.containerComponent}>{children}</div>;
+};
+
+const FormComponent: React.FC = ({ children }) => {
+  return <form action="">{children}</form>;
+};
+
+const TitleContainer = () => {
+  return (
+    <div className={styles['title-container']}>
+      <TitleAuth>Sing In</TitleAuth>
+      <LinkTextCustom bold tiny>
+        Sing up
+      </LinkTextCustom>
+    </div>
+  );
+};
 const SignIn = () => {
   return (
     <SectionComponent>
-      <div></div>
+      <LogoMainSVG width="20.524rem" height="4.3rem" />
+      <TitleContainer />
+      <ContainerComponent>
+        <FormComponent></FormComponent>
+      </ContainerComponent>
     </SectionComponent>
   );
 };
