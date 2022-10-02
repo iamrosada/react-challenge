e1f32e243c7429d2d02fc60bce850773e04184e1
iamrosada <luisrosada2018@gmail.com>
2022-10-02 22:41:20 +0300

    (FEATURE): added the SingUp to Routes

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { RoutesOfAllPage } from '../constant/routesOfAllPage';
+import SignUpPage from '../pages/SignUpPage';
 import SingIn from '../pages/SingIn/SingIn';
 
 const Router: React.FC = () => {
   return (
     <Routes>
       <Route path={RoutesOfAllPage.SignIn} element={<SingIn />} />
+      <Route path={RoutesOfAllPage.SignUp} element={<SignUpPage />} />
     </Routes>
   );
 };
