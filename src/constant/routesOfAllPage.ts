c3446939a31a3c79a3a3fcabffb61ecda51e9bdb
iamrosada <luisrosada2018@gmail.com>
2022-10-02 16:16:12 +0300

    (FEATURE): created the enum with all Routes of Pages

---
+++ src/constant/routesOfAllPage.ts
@@ -0,0 +1,10 @@
+enum RoutesOfAllPage {
+  SignIn = '/sign-in',
+  SignUp = '/sign-up',
+  ForgotPassword = '/forgot-password',
+  Journals = '/journals',
+  CreateJournal = '/journals/create',
+  JournalEntries = '/journals/entries',
+  CreateJournalPost = 'journals/entries/create',
+}
+export { RoutesOfAllPage }
