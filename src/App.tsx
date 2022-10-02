600e297d8eda2b9bee0fd041e8eba9cafd54f5e4
iamrosada <luisrosada2018@gmail.com>
2022-10-02 16:18:47 +0300

    (FEATURE): added the BrowserRouter in the main file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import BackgroundMain from './components/container/Background/background';
+import Router from './routes/router';
 import './styles/scss/_main.scss';
 
 function App() {
-  return <BackgroundMain></BackgroundMain>;
+  return (
+    <BrowserRouter>
+      <BackgroundMain>
+        <Router />
+      </BackgroundMain>
+    </BrowserRouter>
+  );
 }
 
 export default App;
