56519230d27417b0e52c1dd97321841eaf33f218
iamrosada <luisrosada2018@gmail.com>
2022-10-03 11:43:57 +0300

    (FEATURE): updated the component. added  the function to get the value from input

--- src/pages/CreateJornal/CreateJornal.tsx
+++ src/pages/CreateJornal/CreateJornal.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
+import imageLogo from '../../assets/images/logo.svg';
+import Button from '../../components/Button/button';
+import styles from './scss/_createjornal.module.scss';
 
-const CreateJornal = () => {
+type journalType = {
+  title?: string;
+  index?: string;
+};
+type InputType = {
+  onChange?: (event: any) => void;
+};
+const Input: React.FC<InputType> = ({ onChange }) => {
   return (
-    <div>
-      <h1>Create Jornal</h1>
+    <input
+      className={styles.input}
+      placeholder="Define title"
+      onChange={onChange}
+    />
+  );
+};
+
+export const Journal = ({ ...props }: journalType) => {
+  return (
+    <div className={styles.journal}>
+      <div className={styles.journal_spine} />
+      <div className={styles.journal__content}>
+        <h1 className={styles.journal__title}>{props.title}</h1>
+      </div>
     </div>
+  );
+};
+const CreateJornal = () => {
+  const [title, setTitle] = React.useState('');
+  return (
+    <main className={styles.containerEmpty}>
+      <header>
+        <img className="logo" src={imageLogo} alt="Logo" />
+      </header>
+      <section>
+        <Journal title={title} />
+        <Input
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+
+        <Button title="Save Journal" />
+      </section>
+    </main>
   );
 };
 
