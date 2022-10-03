ee9ec9fd7a990fc40ffc260c7a1bf2467743429c
iamrosada <luisrosada2018@gmail.com>
2022-10-03 19:49:46 +0300

    (FEATURE): updated the SingIn Page

--- src/pages/SingIn/SingIn.tsx
+++ src/pages/SingIn/SingIn.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Axios from 'axios';
+import React, { FormEvent, useContext } from 'react';
 import { LogoMainSVG } from '../../assets/svgs';
 import Button from '../../components/Button/button';
 import { Input } from '../../components/Input/input-auth';
 import LinkTextCustom from '../../components/LinkTextCustom/LinkTextCustom';
 import TitleAuth from '../../components/TitleAuth/titleAuth';
+import { AuthContext } from '../../context/userAuthContext';
 
 import styles from './scss/_singIn.module.scss';
 
@@ -16,6 +19,32 @@ const ContainerComponent: React.FC = ({ children }) => {
 };
 
 const FormComponent: React.FC = () => {
+  const { setIsUserLogged, setUserId } = useContext(AuthContext);
+
+  const handleSubmitSigIn = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const UserData = {
+      name: formData.get('username') as string,
+      password: formData.get('password') as string,
+    };
+    try {
+      const response = await Axios.post(
+        'https://fuerza.test/auth/login',
+        UserData
+      );
+      console.log(response.status);
+      if (response.status === 201) {
+        window.location.href = '/journals/create';
+        setUserId(response.data.user.id);
+        setIsUserLogged(true);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <section className={styles['container-form']}>
       <form
@@ -23,10 +52,21 @@ const FormComponent: React.FC = () => {
           display: 'flex',
           flexDirection: 'column',
         }}
+        onSubmit={handleSubmitSigIn}
       >
-        <Input label="Your username" type="text" />
+        <Input
+          label="Your username"
+          type="text"
+          name="username"
+          onChange={() => {}}
+        />
 
-        <Input label="Your Password" type="password" />
+        <Input
+          label="Your Password"
+          type="password"
+          name="password"
+          onChange={() => {}}
+        />
         <LinkTextCustom tiny is to="/">
           Forgot password?
         </LinkTextCustom>
@@ -47,7 +87,7 @@ const TitleContainer = () => {
   return (
     <div className={styles['title-container']}>
       <TitleAuth>Sign in</TitleAuth>
-      <LinkTextCustom bold tiny to="/">
+      <LinkTextCustom bold tiny to="/sign-up">
         Sing up
       </LinkTextCustom>
     </div>
