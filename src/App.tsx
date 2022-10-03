63be9456cdc6390fe0897d902797420adc0715d0
iamrosada <luisrosada2018@gmail.com>
2022-10-03 20:35:02 +0300

    (FEATURE): added the UserAuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import BackgroundMain from './components/container/Background/background';
+import { UserAuthProvider } from './context/userAuthContext';
 import Router from './routes/router';
 import './styles/scss/_main.scss';
 
 function App() {
   return (
     <BrowserRouter>
-      <BackgroundMain>
-        <Router />
-      </BackgroundMain>
+      <UserAuthProvider>
+        <BackgroundMain>
+          <Router />
+        </BackgroundMain>
+      </UserAuthProvider>
     </BrowserRouter>
   );
 }
