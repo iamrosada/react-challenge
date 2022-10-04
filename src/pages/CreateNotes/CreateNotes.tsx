b4ad283d842c88d192e13f40d9b2fa4d92d4dcbc
iamrosada <luisrosada2018@gmail.com>
2022-10-04 13:09:15 +0300

    (FEATURE) : page to create the notes

---
+++ src/pages/CreateNotes/CreateNotes.tsx
@@ -0,0 +1,104 @@
+import React, { useContext, useState } from 'react';
+import imageLogo from '../../assets/images/logo.svg';
+import backLogo from '../../assets/images/back.svg';
+import Button from '../../components/Button/button';
+import styles from './scss/_createNotes.module.scss';
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+import { AuthContext } from '../../context/userAuthContext';
+import { JournalContext } from '../../context/JournalContext';
+
+type NotesType = {
+  content?: string;
+  onChange?: (event: any) => void;
+};
+type InputType = {
+  onChange?: (event: any) => void;
+};
+const Input: React.FC<InputType> = ({ onChange }) => {
+  return (
+    <input
+      className={styles.input}
+      placeholder="Define title"
+      onChange={onChange}
+    />
+  );
+};
+
+export const Notes = ({ onChange }: NotesType) => {
+  return (
+    <textarea
+      className={styles.content_textarea}
+      rows={10}
+      placeholder="Write your note"
+      onChange={onChange}
+    />
+  );
+};
+const CreateNotes = () => {
+  const navigate = useNavigate();
+
+  const { userId } = useContext(AuthContext);
+  const { journalName } = useContext(JournalContext);
+
+  const [title, setTitle] = useState<string>();
+  const [content, setContent] = useState<string>();
+  let { id }: any = useParams();
+
+  const handleNavigate = () => {
+    navigate(`/journal/${id}/posts`);
+  };
+  const handleCreateEntry = async () => {
+    if (title && content) {
+      console.log(title, content);
+      //const noteData = { title: title, content: content, userId: userId };
+      try {
+        const response = await axios.post(
+          `https://fuerza.test/journals/entry/${id}`,
+          {
+            title,
+            content,
+            userId,
+          }
+        );
+        if (response.status === 201) {
+          navigate(`/journal/${id}/posts`);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      alert('Title and content are required');
+    }
+  };
+
+  return (
+    <main className={styles.containerEmpty}>
+      <header>
+        <img className="logo" src={imageLogo} alt="Logo" />
+        <div onClick={handleNavigate}>
+          <img className="logo" src={backLogo} alt="back logo" />
+          <h2>{journalName}</h2>
+        </div>
+      </header>
+      <section>
+        <Input
+          onChange={(e) => {
+            setTitle((e.target as HTMLInputElement).value);
+          }}
+        />
+        <Notes onChange={(e) => setContent(e.target.value)} />
+
+        <Button
+          title="Save a note"
+          onClick={(e) => {
+            e.preventDefault();
+            handleCreateEntry();
+          }}
+        />
+      </section>
+    </main>
+  );
+};
+
+export default CreateNotes;
