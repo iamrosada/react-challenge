ca523dbc4a721876e61341ad62e5b890ed230848
iamrosada <luisrosada2018@gmail.com>
2022-10-04 13:12:11 +0300

    (FEATURE): page jornalListEmpty. when there are no newspapers, this page is shown

--- src/pages/JornalListEmpty/JornalListEmpty.tsx
+++ src/pages/JornalListEmpty/JornalListEmpty.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import imageEmpty from '../../assets/images/image-empty.svg';
+import imageLogo from '../../assets/images/logo.svg';
+import LinkTextCustom from '../../components/LinkTextCustom/LinkTextCustom';
+import styles from './scss/_jornalempty.module.scss';
 
 const JornalListEmpty = () => {
-  return <h1>JornalListEmpty</h1>;
+  return (
+    <main className={styles.containerEmpty}>
+      <header>
+        <img className="logo" src={imageLogo} alt="Logo" />
+      </header>
+      <section>
+        <img
+          className="image_"
+          src={imageEmpty}
+          alt="Imagem para listas vazias"
+        />
+        <LinkTextCustom bold tiny to="/journals/create">
+          Create a journal
+        </LinkTextCustom>
+      </section>
+    </main>
+  );
 };
 export default JornalListEmpty;
