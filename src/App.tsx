807a934e018c5476faf579e70513fd03937e46b2
iamrosada <luisrosada2018@gmail.com>
2022-10-04 23:39:14 +0300

    (FEATURE): added the JournalProvider and ToastContainer to App main

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import BackgroundMain from './components/container/Background/background';
+import { JournalProvider } from './context/JournalContext';
 import { UserAuthProvider } from './context/userAuthContext';
 import Router from './routes/router';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './styles/scss/_main.scss';
 
 function App() {
   return (
     <BrowserRouter>
       <UserAuthProvider>
-        <BackgroundMain>
-          <Router />
-        </BackgroundMain>
+        <JournalProvider>
+          <BackgroundMain>
+            <Router />
+            <ToastContainer />
+          </BackgroundMain>
+        </JournalProvider>
       </UserAuthProvider>
     </BrowserRouter>
   );
