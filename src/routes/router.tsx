c70ec140acc65cdb7a64ef43e0854a7b0a285ac6
iamrosada <luisrosada2018@gmail.com>
2022-10-05 11:36:20 +0300

    (): router private. was in trash, I had forgotten to push

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { RoutesOfAllPage } from '../constant/routesOfAllPage';
+import { AuthContext } from '../context/userAuthContext';
 import CreateJornal from '../pages/CreateJornal/CreateJornal';
 import CreateNotes from '../pages/CreateNotes/CreateNotes';
 import JornalListEmpty from '../pages/JornalListEmpty/JornalListEmpty';
@@ -9,26 +10,72 @@ import NotesListItem from '../pages/NoteListItem/NotesListItem';
 import SignUpPage from '../pages/SignUpPage/SignUpPage';
 import SingIn from '../pages/SingIn/SingIn';
 
+export const ProtectedRoute = ({ children }: any) => {
+  const { userId } = React.useContext(AuthContext);
+
+  if (!userId) {
+    // user is not authenticated
+    return <Navigate to="/" />;
+  }
+  return children;
+};
+
 const Router: React.FC = () => {
   return (
     <Routes>
       <Route index element={<SingIn />} />
       <Route path={RoutesOfAllPage.SignUp} element={<SignUpPage />} />
-
       <Route
         path={RoutesOfAllPage.CreateJournalPost}
-        element={<JornalListEmpty />}
+        element={
+          <ProtectedRoute>
+            <JornalListEmpty />
+          </ProtectedRoute>
+        }
       />
 
-      <Route path={RoutesOfAllPage.CreateJournal} element={<CreateJornal />} />
+      <Route
+        path={RoutesOfAllPage.CreateJournal}
+        element={
+          <ProtectedRoute>
+            <CreateJornal />
+          </ProtectedRoute>
+        }
+      />
 
       <Route
         path={RoutesOfAllPage.JournalEntries}
-        element={<JornalListItem />}
+        element={
+          <ProtectedRoute>
+            <JornalListItem />
+          </ProtectedRoute>
+        }
       />
 
-      <Route path={RoutesOfAllPage.CreateNotes} element={<CreateNotes />} />
-      <Route path={RoutesOfAllPage.NotesListItem} element={<NotesListItem />} />
+      <Route
+        path={RoutesOfAllPage.CreateNotes}
+        element={
+          <ProtectedRoute>
+            <CreateNotes />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path={RoutesOfAllPage.NotesListItem}
+        element={
+          <ProtectedRoute>
+            <NotesListItem />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: '3rem', fontSize: '10rem' }}>
+            <p> Hi, there is nothing here!</p>
+          </main>
+        }
+      />
     </Routes>
   );
 };
